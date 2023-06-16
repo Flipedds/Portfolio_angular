576618874226650c7f6c94f6140929884e788b8b
Filipe André <flipeandre12@gmail.com>
2023-06-16 19:12:36 -0300

    feat: cria template

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { FooterComponent } from './components/footer/footer.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
