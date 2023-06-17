d7f86fea0ba61387b0fdf9677d11b562f2190ddf
Filipe André <flipeandre12@gmail.com>
2023-06-16 23:41:40 -0300

    feat: adiciona novas fotos, e arquivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {MainPageComponent} from "./components/main-page/main-page.component";
+import {SobreMimComponent} from "./components/sobre-mim/sobre-mim.component";
+import {SkillsComponent} from "./components/skills/skills.component";
+import {ProjetosComponent} from "./components/projetos/projetos.component";
 
 
 
 const routes:Routes = [
   {path:'', component: MainPageComponent},
+  {path: 'sobremim', component: SobreMimComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'projetos', component: ProjetosComponent}
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,25 @@ import { HeaderComponent } from './components/header/header.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import {AppRoutingModule} from "./app-routing.module";
 import { FooterComponent } from './components/footer/footer.component';
+import { SobreMimComponent } from './components/sobre-mim/sobre-mim.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import {NgOptimizedImage} from "@angular/common";
+import { ProjetosComponent } from './components/projetos/projetos.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainPageComponent,
-    FooterComponent
+    FooterComponent,
+    SobreMimComponent,
+    SkillsComponent,
+    ProjetosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
