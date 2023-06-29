3d3895f032fc9abfccf7672ac9653c4c62e7b876
Filipe André <flipeandre12@gmail.com>
2023-06-28 21:36:04 -0300

    feat: adiciona responsividade de teste.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isDivVisible: boolean = false;
+  close_menu() {
+    this.isDivVisible = false;
+  }
 
+  openMenu() {
+    this.isDivVisible = true;
+  }
 }
